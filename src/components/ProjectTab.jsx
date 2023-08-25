81f7140fc8d60d1de95dc67b11ca9d40ba465bb4
Ifeanyi Aladi <benedictaladi@gmail.com>
2023-08-26 00:42:17 +0100

    updated project tab

--- src/components/ProjectTab.jsx
+++ src/components/ProjectTab.jsx
@@ -1,6 +1,15 @@
 import React, {useRef, useEffect, useState} from 'react'
 
 const ProjectTab = ({img, alt, heading, text, demo, ghub}) => {
+  const style = {
+    justifyContent: ''
+  }
+  if (ghub) {
+    style.justifyContent = 'space-between'
+  }
+  else {
+    style.justifyContent = 'center'
+  }
   return (
       <div className='pb-6 flex flex-col flex-1 rounded-lg items-center bg-black'>
                 <img src={img} alt={alt} className='w-full rounded-t-lg' />
@@ -8,9 +17,9 @@ const ProjectTab = ({img, alt, heading, text, demo, ghub}) => {
                 <div className='bg-secondary h-[4px] w-[60px] mt-3'></div>
                 <p className='text-dullWhite text-[18px] font-normal text-center w-[290px] mt-6'>{text}</p>
 
-                <div className='flex flex-1 justify-between w-[200px] mt-12'>
+                <div className='flex flex-1 w-[200px] mt-12' style={style}>
                   <a href={demo} target='_blank'><button  className='rounded-3xl py-1 px-2 w-fit text-white bg-secBtn'>Demo</button></a>
-                  <a href={ghub} target='_blank'><button className='rounded-3xl py-1 px-2 w-fit text-white bg-secBtn'>Code</button></a>
+                  {ghub && <a href={ghub} target='_blank'><button className='rounded-3xl py-1 px-2 w-fit text-white bg-secBtn'>Code</button></a>}
                 </div>
       </div>
   )
