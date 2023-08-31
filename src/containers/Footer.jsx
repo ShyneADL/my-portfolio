9c2f8aba5c7cb1d0bb23e6b4a150612699da98a2
Ifeanyi Aladi <benedictaladi@gmail.com>
2023-08-31 01:59:46 +0100

    updated form

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -6,11 +6,11 @@ const Footer = () => {
     <div>
 
       <div className='flex flex-1 justify-center items-center w-full' id='contact'>
-        <form className='flex flex-1 flex-col max-w-[600px] w-[90%] '>
+        <form action="https://formsubmit.co/benedictaladi@gmail.com" method='POST' className='flex flex-1 flex-col max-w-[600px] w-[90%] '>
           <h3 className='text-[32px] font-medium  text-white'>Get In Touch</h3>
-          <input type="text" id="name" placeholder="Name" className='my-2 p-2 rounded-sm' required />
-          <input type="email" id="email" placeholder="Email Address" required className='my-2 p-2 rounded-sm' />
-          <textarea id="message" rows="4" placeholder="Got a job or project proposition?" className='my-2 p-3 rounded-sm'></textarea>
+          <input type="text" name="name" id="name" placeholder="Name" className='my-2 p-2 rounded-sm' required />
+          <input type="email" name="email" id="email" placeholder="Email Address" required className='my-2 p-2 rounded-sm' />
+          <textarea name="textarea" id="message" rows="4" placeholder="Got a job or project proposition?" className='my-2 p-3 rounded-sm'></textarea>
           <button type="submit" className='p-2 bg-secondary text-white text-[18px] border-none outline-none cursor-pointer w-[150px] rounded'>Submit</button>
         </form>
 
