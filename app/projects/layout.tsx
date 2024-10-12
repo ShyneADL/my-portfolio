5327f2fd0d32190dfce08fe6bf4ef27860e8fde3
ShyneADL <benex2002@gmail.com>
2024-10-12 01:24:00 +0100

    added ketohub project

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Footer, Navbar } from "@/app/containers";
 import "../globals.css";
 
@@ -18,6 +19,7 @@ export default function ProjectsLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
