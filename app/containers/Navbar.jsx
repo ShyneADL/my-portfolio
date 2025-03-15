64c045dacd248db75be7e7c199dfa497a906aa8e
ShyneADL <benex2002@gmail.com>
2025-03-15 11:07:37 +0100

    refactor navbar: adjust layout and alignment for improved responsiveness

--- app/containers/Navbar.jsx
+++ app/containers/Navbar.jsx
@@ -31,18 +31,9 @@ const Navbar = () => {
 
   return (
     <div className="fixed bg-[#2a3439] top-0 left-0 w-full z-50">
-      <div className="max-w-[1440px] mx-auto xl:px-[180px] md:px-[60px] md:py-6 px-[16px]">
-        <nav className={`flex flex-1 justify-between items-center`}>
-          <div className="flex justify-start max-w-60px">
-            <Image
-              className="md:w-[60px] w-[45px]"
-              src="/assets/logo.png"
-              alt="logo"
-              width={45}
-              height={45}
-            />
-          </div>
-          <ul className="flex-1 sm:flex hidden justify-end items-center list-none ">
+      <div className="max-w-[1440px] mx-auto xl:px-[180px] md:px-[60px] md:py-10 px-[16px]">
+        <nav className={`flex flex-1 justify-center items-center`}>
+          <ul className="flex-1 sm:flex hidden justify-center items-center list-none ">
             <li
               className={`mr-5 cursor-pointer text-[18px] font-medium ${
                 active === "/" ? "text-white" : "text-[#848482]"
@@ -60,7 +51,7 @@ const Navbar = () => {
           </ul>
 
           {/* Mobile Nav */}
-          <div className="sm:hidden p-4 flex flex-1 justify-end items-center">
+          <div className="sm:hidden p-4 flex flex-1 justify-center items-center">
             <img
               src={toggle ? "/assets/close.svg" : "/assets/menu.svg"}
               alt="menu"
